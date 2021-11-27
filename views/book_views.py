a9a9bede2f24a084e5e25a97b6a1de79c329b0da
yeseul <khys0079@gmail.com>
2021-11-28 03:33:01 +0900

    리뷰 작성 제한 에러

--- views/book_views.py
+++ views/book_views.py
@@ -1,6 +1,6 @@
 from flask  import Blueprint, request, session, redirect, url_for, flash,render_template
 from db_connect import db
-from models.models import Book,BookReview,User
+from models.models import Book,BookReview,BookRental
 from datetime import datetime
 
 bp=Blueprint('book',__name__,url_prefix='/book')
@@ -20,36 +20,32 @@ def book_indetail(book_id):
     db.session.commit()
 
     page = request.args.get('page', type=int, default=1)  # 페이지
-
     review_info =review_info.paginate(page, per_page=8)
 
     return render_template('book_indetail.html',book_id=book_id, book= book_info,review_info=review_info,avg=avg)
     
 
-@bp.route('/wr_review/<int:book_id>', methods=['GET', 'POST'])
+@bp.route('/wr_review/<int:book_id>', methods=('POST',))
 def create_review(book_id):
-    if request.method == 'POST':
-        if 'user_id' not in session:
-            flash('권한이 없습니다.')
-            return redirect(url_for('main.home'))
+    if 'user_id' not in session:
+        flash('권한이 없습니다.')
+        return redirect(url_for('main.home'))
 
-        user_id = session['user_id']
+    user_id = session['user_id']
 
-        # 대여자 중 최초 작성자에 한해
-        if BookReview.query.filter((BookReview.user_id==user_id) & (BookReview.book_id==book_id)).first() is None:
+    # 대여자 중 최초 작성자에 한해
+    if BookReview.query.filter((BookReview.user_id==user_id) & (BookReview.book_id==book_id)).first() is None :
+        review_content=request.form.get('content',None)
+        review_rating =request.form.get('rating',None)
+        review= BookReview(user_id=user_id , book_id=book_id, star=review_rating, comment=review_content,comment_date=datetime.now())
+        db.session.add(review)
+        db.session.commit()
+        flash('리뷰 등록 완료')
+        return redirect(url_for('book.book_indetail', book_id=book_id))
 
-            review_content=request.form.get('content',None)
-            review_rating =request.form.get('rating',None)
-            review= BookReview(user_id=user_id , book_id=book_id, star=review_rating, comment=review_content,comment_date=datetime.now())
-            db.session.add(review)
-            db.session.commit()
-
-            flash('리뷰 등록 완료')
-            return redirect(url_for('book.book_indetail', book_id=book_id))
-        flash('이미 작성하셨습니다.')
-        return redirect(url_for('book.book_indetail',book_id=book_id))
-
-    return redirect('/')
+    flash('이미 작성하셨습니다.')
+    return redirect(url_for('book.book_indetail',book_id=book_id))
+    
 
 @bp.route('/del_review/<int:review_id>')
 def del_review(review_id):    
