a67dc66ae82643e4f634af1e3dcec3996d4e6cc7
yeseul <khys0079@gmail.com>
2021-11-30 03:33:08 +0900

    search 기능 추가

--- views/main_view.py
+++ views/main_view.py
@@ -9,11 +9,21 @@ def home():
     if no==0:
         import load_data
         load_data.load_data()
-    page = request.args.get('page', default=1, type=int)
-    book_list = Book.query.order_by(Book.id.asc())
-    book_list=book_list.paginate( page, per_page=8)
+    page = request.args.get('page', type=int, default=1)
+    kw = request.args.get('kw', type=str, default='')
+    # 조회
+    book_list = Book.query.order_by(Book.title.asc())
+    if kw:
+        search = '%%{}%%'.format(kw)
+        book_list = book_list.filter(
+                    Book.title.ilike(search) |  # 책 제목
+                    Book.author.ilike(search) |  # 저자
+                    Book.description.ilike(search) |  # 설명
+                    Book.isbn.ilike(search) ).order_by(Book.title.asc())   #isbn              
+    # 페이징
+    book_list = book_list.paginate(page, per_page=8)
 
-    return render_template('home.html', book_list=book_list )
+    return render_template('search_result.html', book_list=book_list, page=page, kw=kw)
 
 
 
